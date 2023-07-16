538d19b9b768b5515012efd589b07b7f376247dd
mxsprings <mary.springer8@gmail.com>
2023-07-15 23:34:15 -0500

    made it work
    YEAHHHHHHH

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
+const namer = require('color-namer')
+
+
+// for (let i = 0; i < names.basic.length; i++) {
+//   const element = names.basic[i];
+  
+//   if (element.name === 'pink'){
+//     console.log(element)
+//   }
+// }
+
+// for (const element of names.basic) {
+//   if (element.name === 'pink'){
+//     console.log(element)
+//   }
+// }
+
+// names.basic.forEach((element) => {
+//   if (element.name === 'pink'){
+//     console.log(element)
+//   }
+// })
+
 const appName = 'isitpink.com'
 console.log('This is ' + appName + '!') 
-console.log(`This is ${appName}`)
+// console.log(`This is ${appName}`)
 
-function isItPink(color) {
-  if (!color){
+function isItPink(hex) {
+  if (!hex){
     throw new Error('be fucking for real')
   }
 
-  const _color = color.toLowerCase()
-
-  const isPink = _color === 'pink' 
-
-  console.log(`is it pink? ${_color} : ${isPink ? 'Yes!' : 'no :('}`)
-
-  if (isPink) {
-    return true
-  }
-  else {
-    return false
-  }
+  const result = namer(hex).basic.find((e) => (e.name === 'pink'))
   
+  const isPink = result.distance <= 10
 
+  console.log(`is it pink? ${hex} : ${isPink ? 'Yes!' : 'no :('}`)
+
+  return isPink
+  
 }
+isItPink('#FFC0CC')
+// const isPinkPink = isItPink('pInK')
 
-const isPinkPink = isItPink('pInK')
-
-console.log(isPinkPink)
+//console.log(isPinkPink)
